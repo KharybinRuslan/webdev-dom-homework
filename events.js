c683a5e3a0eac1a18b198a3e7380ef4bdeb3c64c
KharybinRuslan <kharyb@bk.ru>
2024-04-26 09:26:56 +0300

    JOpimization module

--- events.js
+++ events.js
@@ -1,3 +1,6 @@
+import { postComment } from './api.js';
+import { renderComments } from './render.js'; // добавлен импорт
+
 document.addEventListener('DOMContentLoaded', () => {
     const deleteButton = document.getElementById('delete-button');
     deleteButton.addEventListener('click', () => {
@@ -5,6 +8,24 @@ document.addEventListener('DOMContentLoaded', () => {
         const lastComment = commentsList.lastElementChild;
         if (lastComment) {
             lastComment.remove();
+        }
+    });
+
+    const writeButton = document.getElementById('writeButton');
+    writeButton.addEventListener('click', () => {
+        const userName = document.getElementById('userName').value.trim();
+        const userComment = document.getElementById('userComment').value.trim();
+        
+        if (userName && userComment) {
+            postComment(userName, userComment)
+                .then((comments) => {
+                    renderComments(comments);
+                })
+                .catch((error) => {
+                    console.error('Ошибка при отправке комментария:', error);
+                });
+        } else {
+            console.error('Пожалуйста, заполните все поля для отправки комментария.');
         }
     });
 });
